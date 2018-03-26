2d7fa809023a409b07982722d187f05ce36b7b33
Olivier P <provostolivier@hotmail.com>
2018-03-26 10:09:48 -0400

    Character + Camera
    Character Prefab created
    Character can move and jump
    Cinemachine added to the project
    I added the character with some basic animations and made the camera follow the character.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,8 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class InputManager : MonoBehaviour {
+[RequireComponent(typeof(PlayerController))]
+public class InputManager : MonoBehaviour
+{
 
-	
+    List<KeyToEventPair> keyEventList = new List<KeyToEventPair>();
+    PlayerController pc;
+    void Start()
+    {
+        pc = GetComponent<PlayerController>();
+        keyEventList.Add(new KeyToEventPair("Move", pc.MovePressed));
+        keyEventList.Add(new KeyToEventPair("Jump", pc.JumpPressed));
+    }
+    
+    void Update()
+    {
+        foreach (KeyToEventPair kv in keyEventList)
+        {
+            try
+            {
+                float result = Input.GetAxis(kv.axisName);
+                if (result != 0)
+                    kv.floatEvent.Invoke(result);
+            }
+            catch
+            {
+                Debug.LogError("Error for key event pair axis: " + kv.axisName);
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class KeyToEventPair
+    {
+        public string axisName;
+        public FloatEvent floatEvent;
+
+        public KeyToEventPair(string _axisName, params UnityAction<float>[] _floatAction)
+        {
+            axisName = _axisName;
+            floatEvent = new FloatEvent();
+            foreach (UnityAction<float> ua in _floatAction)
+                floatEvent.AddListener(ua);
+        }
+    }
+
+    [System.Serializable]
+    public class FloatEvent : UnityEvent<float> { }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
-	
+    Rigidbody2D rb;
+    Vector2 forceToAdd;
+    public float maxSpeed;
+    public float playerSpeed = 1f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        forceToAdd = new Vector2();
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.AddForce(forceToAdd);
+        rb.position += forceToAdd;
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        Debug.Log(forceToAdd);
+        forceToAdd = new Vector2();
+    }
+
+    public void MovePressed(float moveDirection)
+    {
+        forceToAdd += Vector2.right * moveDirection * playerSpeed;
+        //Debug.Log("Move Pressed : " + moveDirection);
+    }
+
+    public void JumpPressed(float jumpForce)
+    {
+
+        forceToAdd += Vector2.up * jumpForce * playerSpeed;
+    }
+
 }
