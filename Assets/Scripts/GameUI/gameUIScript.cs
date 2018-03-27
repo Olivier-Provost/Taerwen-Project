ef6baca3bbbd7cc74beae300113436b50c793b49
f0rzero <sam_hockey_34@hotmail.ca>
2018-03-27 00:58:24 -0400

    UI time + coin on map
    Add somes coins who can be collected + i had a timer in the top left to know how much time we spend in the level

---
+++ Assets/Scripts/GameUI/gameUIScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameUIScript : MonoBehaviour {
+    public float second,minutes;
+    public Text counterText;
+
+	// Use this for initialization
+	void Start () {
+        counterText = GetComponent<Text>() as Text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        minutes = (int)(Time.time / 60f);
+        second = (int)(Time.time % 60f);
+        counterText.text = minutes.ToString("00") + ":" + second.ToString("00");
+	}
+}
